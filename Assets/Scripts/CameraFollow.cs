0c3882fb84d4f3e70d703f86aa0024efb5733f0d
Orcrox Zyvith <Orcrox_Zyvith@hotmail.com>
2021-12-13 10:31:52 -0600

    Camera Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,15 +6,23 @@ public class CameraFollow : MonoBehaviour {
     private Transform target;
     private Vector3 offset;
 
+    [SerializeField, Range(1, 10)]
+    private float smoothFactor;
+
     void Start() {
         target = GameManager.Instance.player.transform;
     }
 
     void FixedUpdate() {
+        follow();
     }
 
     private void follow() {
-        transform.position = target.position + (Vector3.back * 10);
+        Vector3 targetPos = target.position + (Vector3.back * 10);
+
+        Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, smoothFactor * Time.fixedDeltaTime);
+
+        transform.position = smoothPos;
     }
 
 }
